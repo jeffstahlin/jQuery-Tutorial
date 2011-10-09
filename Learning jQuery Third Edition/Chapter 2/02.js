50a9d2c2edda3c4fddcbc8105b08f5a5fa61cc55
Jeff Stahlin <jeffstahlin@gmail.com>
2011-10-09 16:22:30 +0200

    Learnin jQuery - Chapter 2 (up to Chaining page 43)

---
+++ Learning jQuery Third Edition/Chapter 2/02.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$('#selected-plays > li').addClass('horizontal');
+	$('#selected-plays li:not(.horizontal)').addClass('sub-level');
+	$('a[href^="mailto:"]').addClass('mailto');
+	$('a[href$=".pdf"]').addClass('pdflink');
+	$('a[href^="http"][href*="henry"]').addClass('henrylink');
+});
+
+$(document).ready(function() {
+	$('tr:nth-child(odd)').addClass('alt');
+	$('td:contains(Henry)').parent().children().addClass('highlight');
+});
+
+$('a').filter(function() {
+	return this.hostname && this.hostname != location.hostname;
+}).addClass('external');
